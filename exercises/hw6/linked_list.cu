c14fa958427c1c8bb220609c0a2a5a4087ee7ff0
Eduardo Reis <3452529+eduardo4jesus@users.noreply.github.com>
2022-06-07 17:37:01 -0400

    HW6: solution linked list

--- exercises/hw6/linked_list.cu
+++ exercises/hw6/linked_list.cu
@@ -21,7 +21,8 @@ struct list_elem {
 template <typename T>
 void alloc_bytes(T &ptr, size_t num_bytes){
 
-  ptr = (T)malloc(num_bytes);
+  cudaMallocManaged(&ptr, num_bytes);
+  // ptr = (T)malloc(num_bytes);
 }
 
 __host__ __device__
